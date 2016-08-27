ba392cbac64336d62e50053fb3026ffd46e1a293
DESKTOP-J0B5MC3\hoffm <hoffmann.jonas95@gmail.com>
2016-08-27 18:10:01 +0200

    add first Ground Textures

---
+++ LD36/Assets/Scripts/LevelLoader/RandomTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTexture : MonoBehaviour {
+
+    public Texture2D[] TextureList;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.SetTexture("_MainTex", TextureList[Random.Range(0, TextureList.Length)]);
+    }
+}
