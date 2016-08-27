feef7b9e12f54bf88d9ef601d6c32bc4051fe813
DESKTOP-J0B5MC3\hoffm <hoffmann.jonas95@gmail.com>
2016-08-28 01:42:05 +0200

    add Door Sounds add Step sounds

--- LD36/Assets/Scripts/Misc/Door.cs
+++ LD36/Assets/Scripts/Misc/Door.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class Door : MonoBehaviour
 {
     public Animator doorAnimC;
+    public AudioSource audioSource;
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void OnTriggerEnter(Collider other)
     {
+        audioSource.Play();
         doorAnimC.SetBool("isOpen", true);
     }
 
     public void OnTriggerExit(Collider other)
     {
+        audioSource.Play();
         doorAnimC.SetBool("isOpen", false);
     }
+
 }
---
+++ LD36/Assets/Scripts/Player/PlayerAudioController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAudioController : MonoBehaviour {
+
+    private bool canPlayStepSound = true;
+    public float playTime = 1f;
+    public AudioClip footStep;
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void StartMove()
+    {
+        if (canPlayStepSound)
+        {
+            // Debug.Log("Play Sound");
+            canPlayStepSound = false;
+            StartCoroutine(waitForNextStepSound(playTime));
+            audioSource.clip = footStep;
+            audioSource.Play();
+        }
+    }
+
+    IEnumerator waitForNextStepSound(float time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        canPlayStepSound = true;
+    }
+}
