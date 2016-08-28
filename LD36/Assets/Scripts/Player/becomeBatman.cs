e55b63bcf4abad9319108895a90233fa5e75d9a8
DESKTOP-J0B5MC3\hoffm <hoffmann.jonas95@gmail.com>
2016-08-28 03:04:28 +0200

    do shit

---
+++ LD36/Assets/Scripts/Player/becomeBatman.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class becomeBatman : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.parent.SetParent(null); // become Batman
+	}
+}
